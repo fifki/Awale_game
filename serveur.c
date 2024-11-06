f08ccd72eef10a2bef0c06946a4c76383f64031d
fifki <f.ifkirne21@gmail.com>
2024-11-06 12:35:04 +0100

    the rules and chat files

---
+++ serveur.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 8080
+#define MAX_CLIENTS 10
+int client_sockets[MAX_CLIENTS];
+
+
+
